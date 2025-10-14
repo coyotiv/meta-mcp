7b757bfb40739fa2a4e4c43bc577654b3f12e11b
ardatricity <ardatricity@gmail.com>
2025-10-14 17:01:59 +0300

    refactor: comment out config_id logic in UserAuthManager

--- src/utils/user-auth.ts
+++ src/utils/user-auth.ts
@@ -348,10 +348,10 @@ export class UserAuthManager {
       state: state,
     });
 
-    const config_id = process.env.META_CONFIG_ID;
-    if (config_id) {
-      params.set("config_id", config_id);
-    }
+    // const config_id = process.env.META_CONFIG_ID;
+    // if (config_id) {
+    //   params.set("config_id", config_id);
+    // }
 
     return `https://www.facebook.com/v23.0/dialog/oauth?${params.toString()}`;
   }
